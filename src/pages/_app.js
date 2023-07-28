25109f7b154bd9f0114f76c0af1978a33e3c84ac
luangelsdorf <luanferreira2136@gmail.com>
2023-07-28 10:25:50 -0300

    Added project font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,18 @@
 /* import 'bootstrap/dist/css/bootstrap.css'; */
-import 'src/styles/purpose.scss';
+import 'src/styles/theme/purpose.scss';
+import localFont from 'next/font/local';
+
+const REM = localFont({ src: '../../public/fonts/REM.ttf' });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${REM.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
