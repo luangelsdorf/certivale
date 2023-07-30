b17c3b45f9ce8ab7a7e0a637001067e7798176ec
luangelsdorf <luanferreira2136@gmail.com>
2023-07-30 14:21:25 -0300

    Added icon importing

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false,
+            svgProps: { fill: 'currentColor' }
+          }
+        }
+      ],
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@icons': path.resolve(__dirname, 'public/images/icons/fa')
+    }
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
